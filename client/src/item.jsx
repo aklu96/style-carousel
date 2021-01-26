fcc2b36910f605e2c15962b7113c75c80fe7cffa
Zachary Cush <zachcushonline@gmail.com>
2021-01-26 15:26:40 -0800

    Remove on hover for main carousel items

--- client/src/item.jsx
+++ client/src/item.jsx
@@ -4,13 +4,9 @@ import styled from 'styled-components';
 const CImg = styled.img`
   width: 900px;
   height: 900px;
-  transition: all 0.2s ease;
-  &:hover {
-    transform:scale(1.1);
-  }
 `;
 
-const Item = ({ style, selected }) => {
+const Item = ({ style }) => {
   const { url } = style;
   return (
     <CImg src={url} alt="Product" />
